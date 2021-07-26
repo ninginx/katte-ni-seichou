c30ae16e31997691edfaa8d575e28deb9bf54e77
ninginx <publictaro@gmail.com>
2021-07-26 09:09:45 +0900

    react introduced in katsunari

---
+++ app/src/App.tsx
@@ -0,0 +1,5 @@
+import { ReactElement } from 'react';
+
+const App = (): ReactElement => <div className="App">aaa</div>;
+
+export default App;
---
+++ app/src/components/presentational/templates/index.tsx
@@ -0,0 +1,8 @@
+// home 画面
+// google calendarを使ってトップに勝成の活動予定を入れたい
+
+import { VFC } from 'react';
+
+const Template: VFC = () => <div>home</div>;
+
+export default Template;
